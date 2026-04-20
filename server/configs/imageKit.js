6818c4a649b08f50f4fcba4f509365759a0591ca
navneet maurya <navneetmaurya2005@gmail.com>
2026-04-21 00:07:50 +0530

    fix: imagekit config and env variables

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,7 +1,17 @@
+// import ImageKit from "@imagekit/nodejs";
+
+// const imageKit = new ImageKit({
+//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+// });
+
+// export default imageKit;
+
 import ImageKit from "@imagekit/nodejs";
 
 const imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 export default imageKit;
